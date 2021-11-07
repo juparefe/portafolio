d7d4e8bcb8966f60f7f25445e735fc186a7c6bf3
Juan Rendon <jp_rendon93@hotmail.com>
2021-11-06 21:05:36 -0500

    Diseño responsive small

--- script/index.js
+++ script/index.js
@@ -26,89 +26,33 @@ btn_night.addEventListener('click', () =>{
     }
 })
 
-//Slider de frases
-const slider = document.querySelector(".slider"),
-cards = Array.from(document.querySelectorAll(".card_phrase"));
+//Tarjetas de Proyectos
+const cards = Array.from(document.querySelectorAll('.card'));
+const descriptions = Array.from(document.querySelectorAll('.container'));
+const buttons = Array.from(document.querySelectorAll('.btn_card'));
 
-let isDragging = false,
-    startPos = 0,
-    currentTranslate = 0,
-    prevTranslate = 0,
-    animationId = 0,
-    currentIndex = 0;
-
-cards.forEach((card, index) =>{
-    //Touch
-    card.addEventListener("touchstart", touchStart(index));
-    card.addEventListener("touchend", touchEnd);
-    card.addEventListener("touchmove", touchMove);
-
-    //Mouse
-    card.addEventListener("mousedown", touchStart(index));
-    card.addEventListener("mouseup", touchEnd);
-    card.addEventListener("mouseleave", touchEnd);
-    card.addEventListener("mousemove", touchMove);
+cards.forEach((card, index) => {
+    card.addEventListener("touchstart", function () {
+        descriptions[index].style.display = "flex";
+    });
+});
+buttons.forEach((button, index) => {
+    button.addEventListener("touchend", function () {
+        descriptions[index].style.display = "none";
+    });
 });
 
-// make responsive to viewport changes
-window.addEventListener('resize', setPositionByIndex);
+//Mostrar y ocultar el formulario de contacto
+const switch_button = document.getElementById("switch-button");
+const form = document.querySelector(".form");
+let checked = false;
 
-//Deshabilitar context menu
-window.oncontextmenu = function (event) {
-    event.preventDefault()
-    event.stopPropagation()
-    return false
-}
-
-function getPositionX(event){
-    return event.type.includes("mouse") ? event.pageX : event.touches[0].clientX;
-}
-
-function touchStart(index) {
-    return function(event) {
-        currentIndex = index;
-        startPos = getPositionX(event);
-        isDragging = true
-        animationId = requestAnimationFrame(animation)
-        slider.classList.add('grabbing')
+switch_button.addEventListener("click",function () {
+    if (!checked) {
+        checked = true;
+        form.style.display = "flex";
+    } else {
+        checked = false;
+        form.style.display = "none";
     }
-};
-
-function touchMove(event) {
-    if (isDragging) {
-        const currentPosition = getPositionX(event);
-        currentTranslate = prevTranslate + currentPosition - startPos;
-    }
-};
-
-function touchEnd() {
-    cancelAnimationFrame(animationId);
-    isDragging = false;
-    const movedBy = currentTranslate - prevTranslate
-
-    // if moved enough negative then snap to next slide if there is one
-    if (movedBy < -50 && currentIndex < (cards.length / 3) - 1) currentIndex += 1
-
-    // if moved enough positive then snap to previous slide if there is one
-    if (movedBy > 50 && currentIndex > 0) currentIndex -= 1
-
-    setPositionByIndex()
-    slider.classList.remove('grabbing')
-};
-
-function animation() {
-    setSliderPosition();
-    if (isDragging) {
-        requestAnimationFrame(animation);
-    }
-}
-
-function setPositionByIndex() {
-    currentTranslate = currentIndex * -(window.innerWidth / 10);
-    prevTranslate = currentTranslate;
-    setSliderPosition();
-}
-
-function setSliderPosition() {
-    slider.style.transform = `translateX(${currentTranslate}px)`;
-}
+})
